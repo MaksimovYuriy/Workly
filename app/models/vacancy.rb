d6035e35778dfa6a7c897516f900b2130e1efa83
Maksimov Yuriy <yura.maksimov.04@mail.ru>
2025-12-04 21:52:47 +0300

    Фильтр вакансий по тэгам

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -2,4 +2,5 @@ class Vacancy < ApplicationRecord
   belongs_to :employer
   has_many :responses
   has_many :vacancy_tags
+  has_many :tags, through: :vacancy_tags
 end
--- app/resources/vacancy_resource.rb
+++ app/resources/vacancy_resource.rb
@@ -7,5 +7,11 @@ class VacancyResource < ApplicationResource
     belongs_to :employer
     has_many :responses
     has_many :vacancy_tags
+    has_many :tags, through: :vacancy_tags
 
+    filter :tag, :string do
+        eq do |scope, value|
+            scope.joins(:tags).where(tags: { name: value }).distinct
+        end
+    end
 end
